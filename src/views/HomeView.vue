abaec86302d6dd4c09a6eacc9dc663f868fcc11a
m <m@mPC.lan>
2024-05-15 00:05:30 +0100

    Attempt 2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,8 +5,8 @@ import TheWelcome from '../components/TheWelcome.vue'
 <template>
   <main >
     <TheWelcome />
-    <Parallaxy :speed="20" :animation="(delta: number) => `transform: translate3d(0, ${delta}px, 0);`">
+  </main>
+    <Parallaxy :speed="20" :animation="(delta) => `transform: translate3d(0, ${delta}px, 0);`">
 		<img src="https://picsum.photos/400">
 	</Parallaxy>
-  </main>
 </template>
