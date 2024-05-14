1d5468454f6a8188cbfaec011562a2c29156f20d
m <m@mPC.lan>
2024-05-15 00:20:43 +0100

    Parallax Works

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,13 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => ['Parallaxy'].includes(tag),
+        }
+      }
+    }),
     vueJsx(),
     VueDevTools(),
   ],
